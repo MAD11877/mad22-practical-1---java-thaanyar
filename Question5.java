b960eb4d4a74628784d379410a83304e494cc0e2
thaanyar <104065476+thaanyar@users.noreply.github.com>
2022-07-10 20:48:53 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,43 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Question5
 {
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
+
+  public static void main(String[] args) {
+
     Scanner in = new Scanner(System.in);
+    //System.out.println("Enter the size of array : ");
+    int number_of = in.nextInt();
     
+    int arr[] = new int[number_of];
+    
+    //System.out.println("Enter the elements of array : ");
+    for(int i=0;i<number_of;i++) {
+      arr[i] = in.nextInt();
+    }
+    
+    //System.out.println("Entered elements are : ");
+    for(int i=0;i<number_of;i++) {
+      //System.out.print(arr[i]+" ");
+    }
+    
+    int maxVal=0, count=0, maxCount=0;
+    
+    for(int i=0;i<number_of;i++) {
+      count = 0;
+      for(int j=0;j<number_of;j++) {
+        if(arr[i] == arr[j]) {
+          count++;
+        }
+      }
+      if (count > maxCount) {
+                      maxCount = count;
+                      maxVal = arr[i];
+                 }
+    }
+    
+    //System.out.println("\nThe mode is "+maxVal+" as there are "+maxCount + " of the number " + maxVal );
+    System.out.println(maxVal);
   }
 }
